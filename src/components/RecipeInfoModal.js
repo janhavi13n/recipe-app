941f57c1dfd70ba7fda163eaab901df9c3a7f6ee
JanhaviNagalkar <janhavinagalkar@gmail.com>
2023-10-27 19:44:10 +0530

    Fixed bug

--- src/components/RecipeInfoModal.js
+++ src/components/RecipeInfoModal.js
@@ -3,80 +3,104 @@ import check from "../assets/checkSquare-icon.svg";
 import uncheck from "../assets/xSquare-icon.svg";
 
 const RecipeInfoModal = ({ recipe, hideRecipeInfo }) => {
+  console.log(recipe.extendedIngredients);
   return (
     <>
       <div className="modal-wrapper"></div>
       <div className="modal-container">
         <div className="modal-content">
-        <div className="d-flex flex-wrap section1">
-          <img src={recipe.image} alt={recipe.title} />
-          <div className="d-flex flex-column align-items-start headings">
-            <h4>{recipe.title}</h4>
-            <div className="d-flex justify-content-between w-100">
-              <p style={{ color: recipe.vegan ? "green" : "red" }}>
-                Vegan :{" "}
-                {recipe.vegan ? (
-                  <img src={check} alt="" />
-                ) : (
-                  <img src={uncheck} alt="" />
-                )}
-              </p>
-              <p className="modal-txt" style={{ color: recipe.dairyFree ? "green" : "red" }}>
-                &nbsp;Dairy Free :{" "}
-                {recipe.dairyFree ? (
-                  <img src={check} alt="" />
-                ) : (
-                  <img src={uncheck} alt="" />
-                )}
-              </p>
-            </div>
+          <div className="d-flex flex-wrap section1">
+            <img src={recipe.image} alt={recipe.title} />
+            <div className="d-flex flex-column align-items-start headings">
+              <h4>{recipe.title}</h4>
+              <div className="d-flex justify-content-between w-100">
+                {!!recipe.vegan ? (
+                  <p style={{ color: recipe.vegan ? "green" : "red" }}>
+                    Vegan :{" "}
+                    {recipe.vegan ? (
+                      <img src={check} alt="" />
+                    ) : (
+                      <img src={uncheck} alt="" />
+                    )}
+                  </p>
+                ) : ""}
+                {!!recipe.dairyFree ? (
+                  <p
+                    className="modal-txt"
+                    style={{ color: recipe.dairyFree ? "green" : "red" }}
+                  >
+                    &nbsp;Dairy Free :{" "}
+                    {recipe.dairyFree ? (
+                      <img src={check} alt="" />
+                    ) : (
+                      <img src={uncheck} alt="" />
+                    )}
+                  </p>
+                ) : ""}
+              </div>
 
-            <div className="d-flex justify-content-between w-100">
-              <p style={{ color: recipe.gutenFree ? "green" : "red" }}>
-                Gluten Free :{" "}
-                {recipe.gutenFree ? (
-                  <img src={check} alt="" />
-                ) : (
-                  <img src={uncheck} alt="" />
-                )}
-              </p>
-              <p className="modal-txt" style={{ color: recipe.vegetarian ? "green" : "red" }}>
-              &nbsp;Vegetarian :{" "}
-                {recipe.vegetarian ? (
-                  <img src={check} alt="" />
-                ) : (
-                  <img src={uncheck} alt="" />
-                )}
-              </p>
-            </div>
+              <div className="d-flex justify-content-between w-100">
+                {!!recipe.gutenFree ? (
+                  <p style={{ color: recipe.gutenFree ? "green" : "red" }}>
+                    Gluten Free :{" "}
+                    {recipe.gutenFree ? (
+                      <img src={check} alt="" />
+                    ) : (
+                      <img src={uncheck} alt="" />
+                    )}
+                  </p>
+                ) : ""}
+                {!!recipe.vegetarian ? (
+                  <p
+                    className="modal-txt"
+                    style={{ color: recipe.vegetarian ? "green" : "red" }}
+                  >
+                    &nbsp;Vegetarian :{" "}
+                    {recipe.vegetarian ? (
+                      <img src={check} alt="" />
+                    ) : (
+                      <img src={uncheck} alt="" />
+                    )}
+                  </p>
+                ) : ""}
+              </div>
 
-            <label>Time to cook: {recipe.readyInMinutes} Min</label>
+              {!!recipe.readyInMinutes ? (
+                <label>Time to cook: {recipe.readyInMinutes} Min</label>
+              ) : (
+                ""
+              )}
+            </div>
           </div>
-        </div>
-        {recipe.extendedIngredients?.length > 0 && (
-          <>
-            <h4 className="text-start modal-txt">Ingredients</h4>
-            <ol className="text-start">
-              {recipe.extendedIngredients.map((q, ind) => (
-                <li key={ind}>{q.original}</li>
-              ))}
-            </ol>
-          </>
-        )}
+          {!!recipe.extendedIngredients &&
+            recipe.extendedIngredients?.length > 0 ? (
+              <>
+                <h4 className="text-start modal-txt">Ingredients</h4>
+                <ol className="text-start">
+                  {!!recipe.extendedIngredients &&
+                    recipe.extendedIngredients.map((q, ind) => (
+                      <li key={ind}>{q.original}</li>
+                    ))}
+                </ol>
+              </>
+            ) : ""}
 
-        {recipe.analyzedInstructions[0] && (
-          <>
-            <h4 className="text-start modal-txt">Instructions</h4>
-            <ol className="text-start">
-              {recipe.analyzedInstructions[0].steps.map((q, ind) => (
-                <li key={ind}>{q.step}</li>
-              ))}
-            </ol>
-          </>
-        )}</div>
-       <div className="modal-footer">
-          <button className="btnFH" title="Close" onClick={hideRecipeInfo}>Close</button>
-        </div> 
+          {!!recipe.analyzedInstructions && recipe.analyzedInstructions[0] ? (
+            <>
+              <h4 className="text-start modal-txt">Instructions</h4>
+              <ol className="text-start">
+                {recipe.analyzedInstructions[0].steps.map((q, ind) => (
+                  <li key={ind}>{q.step}</li>
+                ))}
+              </ol>
+            </>
+          ) : ""}
+        </div>
+        <div className="modal-footer">
+          <button className="btnFH" title="Close" onClick={hideRecipeInfo}>
+            Close
+          </button>
+        </div>
       </div>
     </>
   );
